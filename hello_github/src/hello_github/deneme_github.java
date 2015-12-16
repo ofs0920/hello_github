9bff558566f448c482d79e1d7d7b371d6e84e9da
Omer Sanlialp <ofs0920@gmail.com>
2015-12-16 13:30:28 -0600

    first test

---
+++ hello_github/src/hello_github/deneme_github.java
@@ -0,0 +1,10 @@
+package hello_github;
+
+public class deneme_github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github");
+	}
+
+}
